2f27a71d8193f4551fb61cecb57b236ed801c4ec
俺不叫化咕龙 <1801943622@qq.com>
2025-07-10 22:29:21 +0800

    Refactor Runtime methods for coroutine management and enhance context switching

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,12 @@ impl Runtime {
     }
 
     pub fn init(&self) {
-        unsafe {
-            RUNTIME = &raw const self as usize;
-        }
+        unsafe { RUNTIME = self as *const _ as usize }
+    }
+
+    pub fn run(&mut self) {
+        while self.t_yield() {}
+        std::process::exit(0);
     }
 
     pub fn t_yield(&mut self) -> bool {
@@ -74,6 +77,13 @@ impl Runtime {
         self.coroutines.len() > 0
     }
 
+    fn t_return(&mut self) {
+        if self.current != 0 {
+            self.coroutines[self.current].state = CoroutineState::Available;
+            self.t_yield();
+        }
+    }
+
     #[cfg(target_os = "windows")]
     fn spawn(&mut self, f: fn()) {
         let available = self
@@ -89,8 +99,10 @@ impl Runtime {
             // Align the stack pointer to 16 bytes
             // This is necessary for Windows x86_64 calling conventions
             let s_ptr = (s_ptr as usize & !0xF) as *mut u8;
-            std::ptr::write(s_ptr.offset(-16) as *mut u64, f as u64);
-            available.ctx.rsp = s_ptr.offset(-16) as u64;
+            std::ptr::write(s_ptr.offset(-16) as *mut u64, guard as u64);
+            std::ptr::write(s_ptr.offset(-24) as *mut u64, skip as u64);
+            std::ptr::write(s_ptr.offset(-32) as *mut u64, f as u64);
+            available.ctx.rsp = s_ptr.offset(-32) as u64;
             available.ctx.stack_start = s_ptr as u64;
             available.ctx.stack_end = available.stack.as_ptr() as u64;
         }
@@ -152,6 +164,18 @@ impl Coroutine {
     }
 }
 
+#[unsafe(naked)]
+unsafe extern "C" fn skip() {
+    naked_asm!("ret")
+}
+
+fn guard() {
+    unsafe {
+        let rt_ptr = RUNTIME as *mut Runtime;
+        (*rt_ptr).t_return();
+    };
+}
+
 pub fn yield_thread() {
     unsafe {
         let rt_ptr = RUNTIME as *mut Runtime;
@@ -173,49 +197,60 @@ pub fn yield_thread() {
 #[cfg(target_arch = "x86_64")]
 unsafe extern "C" fn swap_ctx() {
     naked_asm!(
-        "movaps [rcx + 0x00], xmm6",
-        "movaps [rcx + 0x10], xmm7",
-        "movaps [rcx + 0x20], xmm8",
-        "movaps [rcx + 0x30], xmm9",
-        "movaps [rcx + 0x40], xmm10",
-        "movaps [rcx + 0x50], xmm11",
-        "movaps [rcx + 0x60], xmm12",
-        "movaps [rcx + 0x70], xmm13",
-        "movaps [rcx + 0x80], xmm14",
-        "movaps [rcx + 0x90], xmm15",
-        "mov    [rcx + 0xa0], rsp  ",
-        "mov    [rcx + 0xa8], r15  ",
-        "mov    [rcx + 0xb0], r14  ",
-        "mov    [rcx + 0xb8], r13  ",
-        "mov    [rcx + 0xc0], r12  ",
-        "mov    [rcx + 0xc8], rbx  ",
-        "mov    [rcx + 0xd0], rbp  ",
-        "mov    [rcx + 0xd8], rdi  ",
-        "mov    [rcx + 0xe0], rsi  ",
-        "movaps  xmm6, [rdx + 0x00]",
-        "movaps  xmm7, [rdx + 0x10]",
-        "movaps  xmm8, [rdx + 0x20]",
-        "movaps  xmm9, [rdx + 0x30]",
-        "movaps  xmm10, [rdx + 0x40]",
-        "movaps  xmm11, [rdx + 0x50]",
-        "movaps  xmm12, [rdx + 0x60]",
-        "movaps  xmm13, [rdx + 0x70]",
-        "movaps  xmm14, [rdx + 0x80]",
-        "movaps  xmm15, [rdx + 0x90]",
-        "mov     rsp,   [rdx + 0xa0]",
-        "mov     r15,  [rdx + 0xa8]",
-        "mov     r14,  [rdx + 0xb0]",
-        "mov     r13,  [rdx + 0xb8]",
-        "mov     r12,  [rdx + 0xc0]",
-        "mov     rbx,  [rdx + 0xc8]",
-        "mov     rbp,  [rdx + 0xd0]",
-        "mov     rdi,  [rdx + 0xd8]",
-        "mov     rsi,  [rdx + 0xe0]",
-        "mov     rax,  [rdx + 0xe8]",
-        "mov     gs:0x08,       rax",
-        "mov     rax,  [rdx + 0xf0]",
-        "mov     gs:0x10,       rax",
-        "ret",
+        r#"
+        movaps [rcx + 0x00], xmm6
+        movaps [rcx + 0x10], xmm7
+        movaps [rcx + 0x20], xmm8
+        movaps [rcx + 0x30], xmm9
+        movaps [rcx + 0x40], xmm10
+        movaps [rcx + 0x50], xmm11
+        movaps [rcx + 0x60], xmm12
+        movaps [rcx + 0x70], xmm13
+        movaps [rcx + 0x80], xmm14
+        movaps [rcx + 0x90], xmm15
+
+        mov    [rcx + 0xa0], rsp
+        mov    [rcx + 0xa8], r15
+        mov    [rcx + 0xb0], r14
+        mov    [rcx + 0xb8], r13
+        mov    [rcx + 0xc0], r12
+        mov    [rcx + 0xc8], rbx
+        mov    [rcx + 0xd0], rbp
+        mov    [rcx + 0xd8], rdi
+        mov    [rcx + 0xe0], rsi
+
+        mov    rax         , gs:0x08
+        mov    [rcx + 0xe8], rax
+        mov    rax         , gs:0x10
+        mov    [rcx + 0xf0], rax
+
+        movaps xmm6        , [rdx + 0x00]
+        movaps xmm7        , [rdx + 0x10]
+        movaps xmm8        , [rdx + 0x20]
+        movaps xmm9        , [rdx + 0x30]
+        movaps xmm10       , [rdx + 0x40]
+        movaps xmm11       , [rdx + 0x50]
+        movaps xmm12       , [rdx + 0x60]
+        movaps xmm13       , [rdx + 0x70]
+        movaps xmm14       , [rdx + 0x80]
+        movaps xmm15       , [rdx + 0x90]
+
+        mov    rsp         , [rdx + 0xa0]
+        mov    r15         , [rdx + 0xa8]
+        mov    r14         , [rdx + 0xb0]
+        mov    r13         , [rdx + 0xb8]
+        mov    r12         , [rdx + 0xc0]
+        mov    rbx         , [rdx + 0xc8]
+        mov    rbp         , [rdx + 0xd0]
+        mov    rdi         , [rdx + 0xd8]
+        mov    rsi         , [rdx + 0xe0]
+        mov    rax         , [rdx + 0xe8]
+        mov    gs:0x08     , rax
+        mov    rax         , [rdx + 0xf0]
+        mov    gs:0x10     , rax
+
+        ret
+    "#
     );
 }
 
@@ -225,12 +260,13 @@ fn main() {
     runtime.init();
     runtime.spawn(|| {
         println!("Coroutine 1 started");
-        // Coroutine logic here
+        yield_thread();
         println!("Coroutine 1 finished");
     });
     runtime.spawn(|| {
         println!("Coroutine 2 started");
-        // Coroutine logic here
+        yield_thread();
         println!("Coroutine 2 finished");
     });
+    runtime.run();
 }
